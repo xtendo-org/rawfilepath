0f5356b10e929a97554e949d24b74e95f39dd198
XT <e@xtendo.org>
2017-01-09 23:58:09 +0900

    Add runProcess.c and FFI

---
+++ src/System/RawFilePath/Process/Internal.hs
@@ -0,0 +1,25 @@
+module System.RawFilePath.Process.Internal where
+
+import Foreign.C
+import Foreign.Ptr
+
+import System.Posix.Types
+import System.Posix.Internals
+
+foreign import ccall unsafe "runInteractiveProcess"
+  c_runInteractiveProcess
+    :: Ptr CString
+    -> CString
+    -> Ptr CString
+    -> FD
+    -> FD
+    -> FD
+    -> Ptr FD
+    -> Ptr FD
+    -> Ptr FD
+    -> Ptr CGid
+    -> Ptr CUid
+    -> CInt                         -- reset child's SIGINT & SIGQUIT handlers
+    -> CInt                         -- flags
+    -> Ptr CString
+    -> IO CPid
