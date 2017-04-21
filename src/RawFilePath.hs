0a348b5059853c71074d215a7188d600817fd930
XT <e@xtendo.org>
2017-04-22 03:59:43 +0900

    Export RawFilePath from RawFilePath

--- src/RawFilePath.hs
+++ src/RawFilePath.hs
@@ -1,6 +1,9 @@
 module RawFilePath
     ( module Module
+    , RawFilePath
     ) where
+
+import System.Posix.ByteString (RawFilePath)
 
 import RawFilePath.Directory as Module
 import RawFilePath.Process as Module
