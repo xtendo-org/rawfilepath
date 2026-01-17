229fd50013f83a2fc04436a58d1a70945c07794d
XT <git@xtendo.org>
2026-01-17 16:31:48 +0900

    Use Hspec for test

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,11 +2,14 @@
 
 import qualified Data.ByteString as B
 import RawFilePath
+import Test.Hspec
 
 main :: IO ()
-main = do
-  p <- startProcess $ proc "echo" ["hello"] `setStdout` CreatePipe
-  result <- B.hGetContents (processStdout p)
-  _ <- waitForProcess p
-
-  print (result == "hello\n")
+main =
+  hspec $
+    describe "RawFilePath.Process" $
+      it "runs echo and reads stdout" $ do
+        p <- startProcess $ proc "echo" ["hello"] `setStdout` CreatePipe
+        result <- B.hGetContents (processStdout p)
+        _ <- waitForProcess p
+        result `shouldBe` "hello\n"
